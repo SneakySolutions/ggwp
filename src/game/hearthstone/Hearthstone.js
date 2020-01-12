05907e4889c8e340f1df4116d0995c7795f7e477
dan <sneakyuhd@hotmail.com>
2020-01-12 22:35:28 +0100

    dan: adddummy brackets to hearthsone, duplicate code. needs refactoring

--- src/game/hearthstone/Hearthstone.js
+++ src/game/hearthstone/Hearthstone.js
@@ -1,11 +1,68 @@
 import React from 'react';
-import '../common/tree.css';
+import '../common/brackets-tree.css';
+import './hearthstone.css';
 
 class Hearthstone extends React.Component {
   render() {
     return (
       <div>
-        HS competition here
+        <div class="bg-image-hearthsone"></div>
+        <div class="brackets">
+          <h2>Gaming tournament test 2019</h2>
+          <turnament id="tournament">
+          <ul class="round round-1">
+            <li class="spacer">&nbsp;</li>
+            
+            <li class="game game-top winner">Sneaky <span>82</span></li>
+            <li class="game game-spacer">&nbsp;</li>
+            <li class="game game-bottom looser">Colo St <span>56</span></li>
+
+            <li class="spacer">&nbsp;</li>
+            
+            <li class="game game-top winner">Oregon <span>74</span></li>
+            <li class="game game-spacer">&nbsp;</li>
+            <li class="game game-bottom looser">Saint Louis <span>57</span></li>
+
+            <li class="spacer">&nbsp;</li>
+            
+            <li class="game game-top looser">Memphis <span>48</span></li>
+            <li class="game game-spacer">&nbsp;</li>
+            <li class="game game-bottom winner">Mich St <span>70</span></li>
+
+            <li class="spacer">&nbsp;</li>
+            
+            <li class="game game-top looser">Creighton <span>50</span></li>
+            <li class="game game-spacer">&nbsp;</li>
+            <li class="game game-bottom winner">Duke <span>66</span></li>
+
+            <li class="spacer">&nbsp;</li>
+          </ul>
+          <ul class="round round-2">
+            <li class="spacer">&nbsp;</li>
+            
+            <li class="game game-top winner">Sneaky <span>77</span></li>
+            <li class="game game-spacer">&nbsp;</li>
+            <li class="game game-bottom looser">Oregon <span>69</span></li>
+
+            <li class="spacer">&nbsp;</li>
+            
+            <li class="game game-top looser">Mich St <span>61</span></li>
+            <li class="game game-spacer">&nbsp;</li>
+            <li class="game game-bottom winner">Duke <span>71</span></li>
+
+            <li class="spacer">&nbsp;</li>
+          </ul>
+          <ul class="round round-3">
+            <li class="spacer">&nbsp;</li>
+            
+            <li class="game game-top winner">Sneaky <span>85</span></li>
+            <li class="game game-spacer">&nbsp;</li>
+            <li class="game game-bottom looser">Duke <span>63</span></li>
+            
+            <li class="spacer">&nbsp;</li>
+          </ul>     
+          </turnament>
+        </div>
       </div>
     )
   }
--- src/game/ow/Overwatch.js
+++ src/game/ow/Overwatch.js
@@ -1,36 +1,37 @@
 import React from 'react';
-import '../common/competition.css';
+import './overwatch.css';
+import '../common/brackets-tree.css';
 
 class Overwatch extends React.Component {
   render() {
     return (
       <div>
-        <div class="bg-image"></div>
+        <div class="bg-image-overwatch"></div>
         <div class="brackets">
           <h2>Gaming tournament test 2019</h2>
-          <main id="tournament">
+          <turnament id="tournament">
           <ul class="round round-1">
             <li class="spacer">&nbsp;</li>
             
-            <li class="game game-top winner">Lousville <span>82</span></li>
+            <li class="game game-top winner">Sneaky <span>82</span></li>
             <li class="game game-spacer">&nbsp;</li>
-            <li class="game game-bottom ">Colo St <span>56</span></li>
+            <li class="game game-bottom looser">Colo St <span>56</span></li>
 
             <li class="spacer">&nbsp;</li>
             
             <li class="game game-top winner">Oregon <span>74</span></li>
             <li class="game game-spacer">&nbsp;</li>
-            <li class="game game-bottom ">Saint Louis <span>57</span></li>
+            <li class="game game-bottom looser">Saint Louis <span>57</span></li>
 
             <li class="spacer">&nbsp;</li>
             
-            <li class="game game-top ">Memphis <span>48</span></li>
+            <li class="game game-top looser">Memphis <span>48</span></li>
             <li class="game game-spacer">&nbsp;</li>
             <li class="game game-bottom winner">Mich St <span>70</span></li>
 
             <li class="spacer">&nbsp;</li>
             
-            <li class="game game-top ">Creighton <span>50</span></li>
+            <li class="game game-top looser">Creighton <span>50</span></li>
             <li class="game game-spacer">&nbsp;</li>
             <li class="game game-bottom winner">Duke <span>66</span></li>
 
@@ -39,13 +40,13 @@ class Overwatch extends React.Component {
           <ul class="round round-2">
             <li class="spacer">&nbsp;</li>
             
-            <li class="game game-top winner">Lousville <span>77</span></li>
+            <li class="game game-top winner">Sneaky <span>77</span></li>
             <li class="game game-spacer">&nbsp;</li>
-            <li class="game game-bottom ">Oregon <span>69</span></li>
+            <li class="game game-bottom looser">Oregon <span>69</span></li>
 
             <li class="spacer">&nbsp;</li>
             
-            <li class="game game-top ">Mich St <span>61</span></li>
+            <li class="game game-top looser">Mich St <span>61</span></li>
             <li class="game game-spacer">&nbsp;</li>
             <li class="game game-bottom winner">Duke <span>71</span></li>
 
@@ -54,13 +55,13 @@ class Overwatch extends React.Component {
           <ul class="round round-3">
             <li class="spacer">&nbsp;</li>
             
-            <li class="game game-top winner">Lousville <span>85</span></li>
+            <li class="game game-top winner">Sneaky <span>85</span></li>
             <li class="game game-spacer">&nbsp;</li>
-            <li class="game game-bottom ">Duke <span>63</span></li>
+            <li class="game game-bottom looser">Duke <span>63</span></li>
             
             <li class="spacer">&nbsp;</li>
           </ul>     
-          </main>
+          </turnament>
         </div>
       </div>
     )
